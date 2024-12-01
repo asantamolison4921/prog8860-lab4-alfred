890c13ba7fffb026d3dda81ba3623b69dc7f2c9d
asantamolison4921 <asantamolison4921@conestogac.on.ca>
2024-11-30 23:06:40 -0500

    initial commit

---
+++ Prog8860Lab4/Prog8860Lab4.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Prog8860Lab4.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal("prog8860-lab4", "prog8860-lab4");
+        }
+    }
+}
---
+++ Prog8860Lab4/Prog8860Lab4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prog8860Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("prog8860-lab4");
+        }
+    }
+}

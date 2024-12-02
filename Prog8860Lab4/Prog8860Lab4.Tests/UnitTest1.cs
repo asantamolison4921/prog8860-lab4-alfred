3c3eed8982477b9a53e1abba8351910c0761a7c5
asantamolison4921 <asantamolison4921@conestogac.on.ca>
2024-12-01 21:45:07 -0500

    mofified application file and test file

--- Prog8860Lab4/Prog8860Lab4.Tests/UnitTest1.cs
+++ Prog8860Lab4/Prog8860Lab4.Tests/UnitTest1.cs
@@ -1,13 +1,28 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Xunit;
+using Moq;
 
 namespace Prog8860Lab4.Tests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public async Task TestFunction1()
         {
-            Assert.Equal("prog8860-lab4", "prog8860-lab4");
+            // Arrange
+            var logger = new Mock<ILogger>();
+            var context = new DefaultHttpContext();
+            var request = context.Request;
+
+            // Act
+            var result = await Prog8860Lab4.Function1.Run(request, logger.Object) as OkObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal("prog8860-lab4", result.Value);
         }
     }
 }
--- Prog8860Lab4/Prog8860Lab4/Program.cs
+++ Prog8860Lab4/Prog8860Lab4/Program.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
 
 namespace Prog8860Lab4
 {
-    class Program
+    public static class Function1
     {
-        static void Main(string[] args)
+        [FunctionName("Function1")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
         {
-            Console.WriteLine("prog8860-lab4");
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string responseMessage = "prog8860-lab4";
+
+            return new OkObjectResult(responseMessage);
         }
     }
 }
